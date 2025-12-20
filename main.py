d305226c71382705e855aadaa1a8deb1ee6c6a0e
danilotec <danilo0123499@gmail.com>
2025-12-20 11:06:38 -0300

    percent function

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from regression import Manometer
 
 man = Manometer("runs/detect/train/weights/best.pt")
-print(man.get_angle("dataset/regression_dataset/images/image_3.jpg"))
+angle = man.get_angle("dataset/regression_dataset/images/image_1.jpg")
 
+def angle_to_percent(angle):
+    if angle < 135:
+        angle += 360
+
+    percentual = (angle - 135) / 270
+    return max(0.0, min(1.0, percentual))
+
+print(angle_to_percent(angle))
