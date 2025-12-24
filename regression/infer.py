4f6b9651df52e5d2f2c71aab66af1ea0457f1d4b
danilotec <danilo0123499@gmail.com>
2025-12-24 08:54:27 -0300

    recebendo o volume diretamente do objeto

--- regression/infer.py
+++ regression/infer.py
@@ -13,8 +13,8 @@ class Manometer:
     def get_angle(self, filename: str) -> list | None:
         img = cv2.imread(filename)
         result = self.yolo(img)[0] #type: ignore
-
-        angles = []
+        result.plot()
+        self.angles = []
 
         for box in result.boxes:
             x1, y1, x2, y2 = map(int, box.xyxy[0])
@@ -24,5 +24,35 @@ class Manometer:
             t = torch.tensor(crop).float().permute(2, 0, 1).unsqueeze(0) / 255
 
             ang_norm = self.reg(t).item()
-            angles.append(ang_norm * 360)
-        return angles
+            self.angles.append(ang_norm * 360)
+        return self.angles
+    
+    def angle_to_percent(self, angle: float) -> float:
+        '''
+        :param angle: esse paramentro deve ser preenchido com o retorno do metodo get_angle da classe manometer
+        :type angle: float
+        
+        :return: retorna o angulo do ponteiro do manometro
+        :rtype: float
+        '''
+
+        if angle < 135:
+            angle += 360
+
+        percentual = (angle - 135) / 270
+        return max(0.0, min(1.0, percentual))
+    
+    def get_volume(self, scale: float) -> float:
+        '''    
+        :param percent: se refere a porcentagem do circulo do manometro
+        esse valor deve vir da função angle_to_percent 
+        :type percent: float
+        
+        :param scale: se refere ao valor maximo de leitura do manometro, que reprezenta o 100%
+        do mesmo
+        :type scale: float
+        
+        :return: retorna o valor real do manometro baseado na posição do ponteiro
+        :rtype: float
+        '''
+        return self.angle_to_percent(self.angles[0]) * scale
